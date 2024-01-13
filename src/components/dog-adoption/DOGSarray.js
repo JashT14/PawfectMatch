4d3adc906dab27ba62aa1e8b877460d7b22f0b12
fatimampg <fatimampgouveia@gmail.com>
2024-01-13 23:44:58 +0000

    feat: add DOGSarray.js and ASSOCIATIONSarray.js
    Dummy data: arrays containing information about dogs for adoption and information about associations, to be used for testing purposes

---
+++ src/components/dog-adoption/DOGSarray.js
@@ -0,0 +1,55 @@
+const DOGSarray = [
+  {
+    dogId: 1,
+    dogName: "Balu",
+    dogBreed: "Golden Retriever",
+    dogAge: "more than 2 years",
+    country: "Portugal",
+    state: "Madeira",
+    city: "Funchal",
+    dogDescription: "blablablablablabla",
+    dogPhotos: [
+      "http://pets-images.dev-apis.com/pets/dog25.jpg",
+      "http://pets-images.dev-apis.com/pets/dog26.jpg",
+      "http://pets-images.dev-apis.com/pets/dog27.jpg",
+      "http://pets-images.dev-apis.com/pets/dog28.jpg",
+      "http://pets-images.dev-apis.com/pets/dog29.jpg",
+    ],
+    associationName: "PawsAdopt", //assign the association adding this dog
+  },
+  {
+    dogId: 2,
+    dogName: "Feijão",
+    dogBreed: "Unknown",
+    dogAge: "less than 6 months",
+    country: "Portugal",
+    state: "Lisbon",
+    city: "Lisbon",
+    dogDescription: "blablablablablabla",
+    dogPhotos: [
+      "http://pets-images.dev-apis.com/pets/dog37.jpg",
+      "http://pets-images.dev-apis.com/pets/dog38.jpg",
+      "http://pets-images.dev-apis.com/pets/dog39.jpg",
+    ],
+    associationName: "PawsAdopt2",
+  },
+  {
+    dogId: 3,
+    dogName: "Luna",
+    dogBreed: "Unknown",
+    dogAge: "more than 2 years",
+    country: "Portugal",
+    state: "Lisbon",
+    city: "Lisbon",
+    dogDescription: "blablablablablabakakakkakakaakbla",
+    dogPhotos: [
+      "http://pets-images.dev-apis.com/pets/dog35.jpg",
+      "http://pets-images.dev-apis.com/pets/dog36.jpg",
+    ],
+    associationName: "PawsAdopt3",
+  },
+];
+
+export default DOGSarray;
+
+//dogAge (options: up to 6 months, 6 months to 2 years, more than 2 years)
