23c1fe4d7c2b377afcf317130f8ef6b9fdf713b6
fatimampg <fatimampgouveia@gmail.com>
2024-01-13 23:55:47 +0000

    refactor: replace <a> tag with <Link> component

--- src/components/dog-adoption/Dog.jsx
+++ src/components/dog-adoption/Dog.jsx
@@ -1,30 +1,31 @@
 import noPhoto from "../../assets/images/noPhoto.png";
+import { Link } from "react-router-dom";
 
-const Dog = (props) => {
+const Dog = ({ dogPhotos, dogId, dogName, dogBreed, location }) => {
   let mainDogPhoto = noPhoto;
 
-  if (props.dogPhotos.length) {
-    mainDogPhoto = props.dogPhotos[0];
+  if (dogPhotos.length) {
+    mainDogPhoto = dogPhotos[0];
   }
   return (
-    <a href={`/details/${props.dogId}`} className="">
+    <Link to={`/dogdetails/${dogId}`} className="">
       <div className="card-results-dogs flex h-[8rem] w-[48.0rem] items-center">
         <div className="float-left ml-[1rem] mr-[1.5rem] h-[5rem] w-[5rem] rounded-full object-cover">
-          <img src={mainDogPhoto} alt={props.dogName} />
+          <img src={mainDogPhoto} alt={dogName} />
         </div>
         <div className="dogs-info">
           <h1 className="text-lightest text-[1.00rem]">
-            Name:&nbsp;&nbsp;&nbsp;{props.dogName}
+            Name:&nbsp;&nbsp;&nbsp;{dogName}
           </h1>
           <h2 className="text-lightest text-[1.00rem]">
-            Breed:&nbsp;&nbsp;&nbsp;{props.dogBreed}
+            Breed:&nbsp;&nbsp;&nbsp;{dogBreed}
           </h2>
           <h2 className="text-lightest text-[1.00rem]">
-            Location:&nbsp;&nbsp;&nbsp;{props.location}
+            Location:&nbsp;&nbsp;&nbsp;{location}
           </h2>
         </div>
       </div>
-    </a>
+    </Link>
   );
 };
 
