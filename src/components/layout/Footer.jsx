d5f82c181b179756a6c1029a7a4edc5a656ee2ce
fatimampg <fatimampgouveia@gmail.com>
2024-01-19 12:30:05 +0000

    style: reduce height of the Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="bg-darkest">
-      <footer className="mx-auto flex h-[5rem] w-[97%] items-center justify-center shadow-inner">
-        <ul className="font-customFont text-lightest flex gap-6 text-[1.0rem] font-semibold md:flex-row md:items-center md:gap-[7vw] ">
+      <footer className="mx-auto flex h-[4.0rem] w-[97%] items-center justify-center shadow-inner">
+        <ul className="font-customFont flex gap-6 text-[1.0rem] font-semibold text-white md:flex-row md:items-center md:gap-[7vw] ">
           <li>
             <NavLink to="/faq">FAQ</NavLink>{" "}
           </li>
