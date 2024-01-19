4d1274ba94203d140ab1ad3a768336761bea563b
fatimampg <fatimampgouveia@gmail.com>
2024-01-19 12:33:37 +0000

    feat: add useNavigate hook for navigation in Navbar component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,14 +1,16 @@
-import { useState, useEffect } from "react";
-import React from "react";
-import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import projectLogo from "../../assets/images/projectLogo.png";
 
 const Navbar = () => {
-  const isLoggedIn = true;
+  const isLoggedIn = false;
   const handleLogout = () => {
-    console.log("is logged in");
+    console.log("is logged out");
+    //TO DO!!!!
   };
+
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
   return (
     <div className="bg-darkest">
@@ -20,9 +22,9 @@ const Navbar = () => {
         <div
           className={`nav-links bg-darkest absolute left-0 ${
             isMenuOpen ? "top-[5.0rem]" : "top-[-100%]"
-          } z-10 flex min-h-[14rem] w-full items-center px-5 md:static md:min-h-[5.0rem] md:w-auto`}
+          } z-10 flex min-h-[14rem] w-full items-center px-5 md:static md:min-h-[4.5rem] md:w-auto`}
         >
-          <ul className="font-customFont text-lightest flex flex-col gap-8 text-[1.0rem] font-semibold md:flex-row md:items-center md:gap-[7vw] ">
+          <ul className="font-customFont flex flex-col gap-8 text-[1.0rem] font-semibold text-white md:flex-row md:items-center md:gap-[7vw] ">
             <li>
               <NavLink to="/home">HOME</NavLink>{" "}
             </li>
@@ -35,7 +37,7 @@ const Navbar = () => {
           </ul>
         </div>
 
-        <div className="font-customFont text-lightest flex w-[18.0rem] justify-end text-[1.0rem] font-semibold lg:w-auto">
+        <div className="font-customFont flex w-[18.0rem] justify-end text-[1.0rem] font-semibold text-white lg:w-auto">
           {isLoggedIn ? (
             <div className="">
               <ul className="flex items-center gap-4">
@@ -44,11 +46,12 @@ const Navbar = () => {
                 </li>
                 <li>
                   <button
-                    className="bg-medium hover:bg-lightest hover:text-darkest rounded-full px-5 py-2"
+                    className="bg-medium hover:text-darkest rounded-full px-5 py-2 hover:bg-white"
                     onClick={handleLogout}
                   >
                     Log Out
                   </button>
+                  {/* WHEN CLICKING LOGOUT THEN DO ....*/}
                 </li>
                 <li className="cursor-pointer text-2xl md:hidden">
                   <ion-icon
@@ -66,8 +69,8 @@ const Navbar = () => {
               <ul className="flex items-center gap-4">
                 <li>
                   <button
-                    className="bg-medium hover:bg-lightest hover:text-darkest text-lightest rounded-full px-5 py-2"
-                    onClick={handleLogout}
+                    className="bg-medium hover:text-darkest rounded-full px-5 py-2 text-white hover:bg-white"
+                    onClick={() => navigate("/login")}
                   >
                     Log In
                   </button>
