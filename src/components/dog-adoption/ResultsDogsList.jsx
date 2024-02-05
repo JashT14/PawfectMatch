1712bd454020cdb3da0a97e69ba3fd0dc1b3f6cf
fatimampg <fatimampgouveia@gmail.com>
2024-02-05 14:57:12 +0000

    feat: add ResultsDogsList.jsx (imported into ManageDogs.jsx)

---
+++ src/components/dog-adoption/ResultsDogsList.jsx
@@ -0,0 +1,27 @@
+import DogCardEditRemove from "./DogCardEditRemove";
+
+const ResultsDogsList = ({ filteredDogsArray }) => {
+  return (
+    <div>
+      <div className="">
+        {!filteredDogsArray.length ? (
+          <h1>No dogs found</h1>
+        ) : (
+          filteredDogsArray.map((dog) => (
+            <DogCardEditRemove
+              dogName={dog.dogName}
+              dogBreed={dog.dogBreed}
+              location={`${dog.country}, ${dog.state}, ${dog.city}`}
+              dogPhotos={dog.dogPhotos}
+              dogId={dog._id}
+              key={dog.dogId}
+              dogProfilePhoto={dog.dogProfilePhoto}
+            />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ResultsDogsList;
